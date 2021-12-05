5b87f5bf5fc6c8901dbda62baa6252224de6d4ca
Azmiroff <Mirsoliev9099@gmail.com>
2021-12-05 14:58:30 +0500

    add sort

--- js/main.js
+++ js/main.js
@@ -1,96 +1,66 @@
+let cutMovies = movies.slice(0,20)
 
-var mainCard = movies.slice(0, 20)
-var elList = document.querySelector("#card");
+let result = cutMovies.sort((a, b) => String(a.Title).toLowerCase() > String(b.Title).toLowerCase()? 1 : -1)
 
-for (let i = 0; i < mainCard.length; i++) {
-     
+
+const elWrapper =document.querySelector("#wrapper");
+
+for (let i = 0; i < cutMovies.length; i++) {
+
     var newList = document.createElement("div");
-    newList.setAttribute("class", "border border-blue p-2 m-3 w-50");
-    var newImg = document.createElement("img")
-    newImg.setAttribute("src", `http://i3.ytimg.com/vi/${mainCard[i].ytid}/maxresdefault.jpg`);
-    newImg.setAttribute("class" , "card-img-top")
+    newList.setAttribute("class" ,  "item border border-black ms-3 mt-2")
 
-    var cardH5 =document.createElement("h5");
-    cardH5.setAttribute("class" , "p-3")
-    cardH5.textContent = mainCard[i].Title;
-
-    var cardH6 = document.createElement("h6");
-    cardH6.textContent = "Year:"+" "+ mainCard[i].movie_year; 
-
-    var cardP = document.createElement("p");
-    cardP.textContent = "Rating:"+" "+ mainCard[i].imdb_rating;
-
-    var cardLink = document.createElement("a");
-    cardLink.setAttribute("class", "btn btn-outline-primary");
-    cardLink.setAttribute("href", `https://www.youtube.com/watch?v=${mainCard[i].ytid}`);
-    cardLink.setAttribute("target", "_blank")
-    cardLink.textContent = "Watch trailer";
-
-    var cardBTN = document.createElement("button");
-    cardBTN.setAttribute("class", "btn btn-outline-secondary ms-1");
-    cardBTN.textContent = "More info";
+    var newImg = document.createElement("img");
+    newImg.setAttribute("src", `http://i3.ytimg.com/vi/${cutMovies[i].ytid}/maxresdefault.jpg`)
+    newImg.setAttribute("class", "img")
 
 
-    var bookmarkBTN = document.createElement("button");
-    bookmarkBTN.setAttribute("class", "btn btn-outline-success ms-2"),
-    bookmarkBTN.textContent = "Bookmarker"
+    var newTextH6 = document.createElement("h6");
+    newTextH6.textContent = cutMovies[i].Title
+    newTextH6.setAttribute("class" , "text-danger ps-3 pt-2");
+
+    var newTextYear = document.createElement("p");
+    newTextYear.textContent ="Year : " + cutMovies[i].movie_year
+    newTextYear.setAttribute("class" , "text-primary ps-3 pt-3");
 
 
-    elList.appendChild(newList);
-    newList.appendChild(newImg);
-    newList.appendChild(cardH5);
-    newList.appendChild(cardH6);
-    newList.appendChild(cardP);
-    newList.appendChild(cardLink);
-    newList.appendChild(cardBTN);
-    newList.appendChild(bookmarkBTN)
-}
+    var newTextRate = document.createElement("p");
+    newTextRate.textContent = "Rating : " + cutMovies[i].imdb_rating
+    newTextRate.setAttribute("class" , "text-warning ps-3 pt-2");
 
-var render = movies.slice(0, 20);
-
-
-var elList = document.querySelector("#list");
-
-for (let i = 0; i < render.length; i++) {
-    
-    var new_List = document.createElement("div");
-    new_List.setAttribute("class", "border border-dark p-2 m-3 w-50");
-    var new_img = document.createElement("img")
-    new_img.setAttribute("src", `http://i3.ytimg.com/vi/${render[i].ytid}/maxresdefault.jpg`);
-    
-    new_img.setAttribute("class" , "card-img-top");
-    
-    var newH5 = document.createElement("h5");
-    newH5.setAttribute("class", "pt-2")
-    newH5.textContent = render[i].Title;
-    
-    var newH6 = document.createElement("h6");
-    newH6.textContent = "Released:"+" "+ render[i].movie_year; 
-
-    var newP = document.createElement("p");
-    newP.textContent = "Reyting:"+" "+ render[i].imdb_rating;
 
     var newLink = document.createElement("a");
-    newLink.setAttribute("class", "btn btn-outline-primary");
-    newLink.setAttribute("href", `https://www.youtube.com/watch?v=${render[i].ytid}`);
+    newLink.setAttribute("href", `https://www.youtube.com/watch?v=${cutMovies[i].ytid}`)
     newLink.setAttribute("target", "_blank")
-    newLink.textContent = "Watch trailer";
+    newLink.setAttribute("class", "btn btn-outline-primary ms-2 mb-2")
+    newLink.textContent = "Watch"
 
-    var btnMore = document.createElement("button");
-    btnMore.setAttribute("class", "btn btn-outline-secondary ms-2");
-    btnMore.textContent = "More info";
+    var newButton = document.createElement("button")
+    newButton.setAttribute("class", "btn btn-outline-success ms-2 mb-2")
+    newButton.textContent = "MORE INFO"
 
-    var btnBookmark = document.createElement("button");
-    btnBookmark.setAttribute("class", "btn btn-outline-success ms-2"),
-    btnBookmark.textContent = "Bookmarker"
+    var newButton11 = document.createElement("button")
+    newButton11.setAttribute("class", "btn btn-outline-danger ms-2 mb-2")
+    newButton11.textContent = "BOOKMARKS"
 
     
-    elList.appendChild(new_List);
-    new_List.appendChild(new_img);
-    new_List.appendChild(newH5);
-    new_List.appendChild(newH6);
-    new_List.appendChild(newP);
-    new_List.appendChild(newLink);
-    new_List.appendChild(btnMore);
-    new_List.appendChild(btnBookmark);
+    elWrapper.appendChild(newList)
+    newList.appendChild(newImg)
+    newList.appendChild(newTextH6)
+    newList.appendChild(newTextYear)
+    newList.appendChild(newTextRate)
+    newList.appendChild(newLink)
+    newList.appendChild(newButton)
+    newList.appendChild(newButton11)
+
+    
 }
+
+
+
+
+
+
+
+
+
